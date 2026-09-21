4e2f234753707e2f0012e9783d802f446ea3c11d
Saul Mendoza <saulmendoza6000@gmail.com>
2026-09-21 00:07:03 -0500

    noshe

--- src/main/java/com/proyectos/BookGestion/controller/plan_controller/PlanController.java
+++ src/main/java/com/proyectos/BookGestion/controller/plan_controller/PlanController.java
@@ -1,11 +1,8 @@
 package com.proyectos.BookGestion.controller.plan_controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.config.PlaceholderConfigurerSupport;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.proyectos.BookGestion.dto.plan_dto.PlanResponseDTO;
 import com.proyectos.BookGestion.dto.plan_dto.PlanSaveDTO;
 import com.proyectos.BookGestion.dto.plan_dto.PlanUpdateDTO;
-import com.proyectos.BookGestion.model.Plan;
 import com.proyectos.BookGestion.service.plan_service.PlanService;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
